2370bb70ed07cdb475ad55d3ce9b9d49ffa7962d
SosisMayo <ramadani.dwicahya@gmail.com>
2021-09-11 20:31:59 +0700

    Page Comingsoon

---
+++ resources/views/coming-soon.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- meta section start -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Ini Lho ITS 2020 adalah sebuah event yang membantu calon mahasiswa baru ITS menemukan jurusan yang sesuai minat & bakat yang dimiliki nya. Inilho ITS Memiliki serangkaian event yang terdiri dari: talkshow inspiratif, open campus, welcome, dan lainnya!">
+    <meta name="keywords" content="ITS, Institut Teknologi Sepuluh Nopember, ILITS, Ini Lho ITS!, ILITS 2022, Ini Lho ITS! 2022">
+    <!-- title section start -->
+    <title>Coming soon!</title>
+    <!-- link section start -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/comingsoon.css">
+</head>
+<body>
+
+        <div class="content text-center">
+            <h1>Coming soon!</h1>
+            <h2><span>INI LHO ITS! 2022</span> akan segera hadir</h2>
+            <h2>Stay tune!</h2>
+        </div>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('coming-soon');
 });
 
 Route::get('test/', [OprecStaffController::class, 'create'])->name('oprec.create');
