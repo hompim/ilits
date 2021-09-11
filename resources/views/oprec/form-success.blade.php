a870e0f4290af5c49d1c721158e4bfddf448bcbb
akmalf06 <akmaldavinci06@gmail.com>
2021-09-11 23:58:07 +0700

    Oprec success page

--- resources/views/oprec/form-success.blade.php
+++ resources/views/oprec/form-success.blade.php
@@ -1,12 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- meta section start -->
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sukses</title>
+    <meta name="description" content="Ini Lho ITS 2020 adalah sebuah event yang membantu calon mahasiswa baru ITS menemukan jurusan yang sesuai minat & bakat yang dimiliki nya. Inilho ITS Memiliki serangkaian event yang terdiri dari: talkshow inspiratif, open campus, welcome, dan lainnya!">
+    <meta name="keywords" content="ITS, Institut Teknologi Sepuluh Nopember, ILITS, Ini Lho ITS!, ILITS 2022, Ini Lho ITS! 2022">
+    <!-- title section start -->
+    <title>Pengumuman!</title>
+    <!-- link section start -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/oprec/oprec-success.css') }}">
 </head>
 <body>
-    <h1>Yes, kamu dah terdaftar</h1>
+
+    <div class="container">
+        <h1>Ini Lho ITS! 2022</h1>
+        <div class="content text-center">
+            <div class="text-content">
+                <h1>Pengumpulan Berkas</h1>
+                <p>Jangan lupa untuk melengkapi pendaftaranmu dengan <br>mengisi berkas - berkas melalui link di bawah ini ya!</p>
+                <a href="https://intip.in/PengumpulanBerkasILITS2022">https://intip.in/PengumpulanBerkasILITS2022</a>
+            </div>
+        </div>
+    </div>
+
+    <script src="{{ mix('js/app.js') }}" defer></script>  
 </body>
 </html>
