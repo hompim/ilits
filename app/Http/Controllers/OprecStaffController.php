6e6441db2930e6779e200ad4dda82a61af8ecc61
akmalf06 <akmaldavinci06@gmail.com>
2021-09-19 04:06:23 +0700

    Solve merge conflict

--- app/Http/Controllers/OprecStaffController.php
+++ app/Http/Controllers/OprecStaffController.php
@@ -120,10 +120,4 @@ class OprecStaffController extends Controller
             return redirect()->back()->withInput()->with('error', $e->getMessage());
         }
     }
-
-    public function anouncement()
-    {
-        $status = "true";   //pass status - true or false
-        return view("oprec.anouncement", compact("status"));
-    }
 }
