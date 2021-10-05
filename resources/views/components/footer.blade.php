4769e2f7873be12bd0235cbe6e0ab8ee7fad279d
christhopermarcelino <christhopermarcelino@gmail.com>
2021-10-05 22:33:54 +0700

    Set up layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,62 @@
+<footer class="text-center text-lg-start text-muted">
+  <section class="container p-4 d-flex justify-content-center justify-content-lg-between">
+    <div class="ml-auto text-left">
+      <p class="text-white sosmed-title">Temukan Kami di Sosial Media!</p>
+      <div class="icons d-flex justify-content-between">
+        <img src="{{ asset('img/icons/ic_world.png') }}" alt="World" class="mx-2" />
+        <img src="{{ asset('img/icons/ic_instagram.png') }}" alt="Instragram" class="mx-2" />
+        <img src="{{ asset('img/icons/ic_twitter.png') }}" alt="Twitter" class="mx-2" />
+        <img src="{{ asset('img/icons/ic_youtube.png') }}" alt="Youtube" class="mx-2" />
+        <img src="{{ asset('img/icons/ic_tiktok.png') }}" alt="Tiktok" class="mx-2" />
+        <img src="{{ asset('img/icons/ic_line.png') }}" alt="Line" class="mx-2" />
+        <img src="{{ asset('img/icons/ic_linkedin.png') }}" alt="LinkedIn" class="mx-2" />
+      </div>
+    </div>
+  </section>
+
+  <section class="container mt-5">
+    <div class="row">
+      <div class="col-md-2">
+        <h6 class="mb-4">
+          Tentang Kami
+        </h6>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+
+      <div class="col-md-2">
+        <h6 class="mb-4">
+          Eventual
+        </h6>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+
+      <div class="col-md-2">
+        <h6 class="mb-4">
+          Try Out
+        </h6>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+
+      <div class="col-md-2">
+        <h6 class="mb-4">
+          Shop
+        </h6>
+      </div>
+  </section>
+
+  <section class="container p-4 text-right">
+    <span>&copy; 2022 Ini Lho ITS!</span>
+  </section>
+</footer>
