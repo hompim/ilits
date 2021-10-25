a984c06ad6430e5ec3d1f32a1fb11f9d8ae43c1b
Muhammad Yunus <Yunusenvire@gmail.com>
2021-10-25 11:32:04 +0700

    Request-Change-Fixation
    Change log
    + simple-two-side-card
    text-align-left.
    mobile viewport adjustment
    + minor fix on other components

--- resources/views/components/card-with-header.blade.php
+++ resources/views/components/card-with-header.blade.php
@@ -3,10 +3,10 @@
     <h3 class="px-2 px-md-3 px-lg-4 card-header" 
     style="background: linear-gradient(45deg, {{$color1}} 0%, {{$color2}} 61.85%, {{$color3}} 88.26%);"
     >{{$title}}</h3>
-    <div class="card-body py-3 px-4">
-        <div class="py-3 px-3 px-md-5">
+    <div class="card-content px-4">
+        <div class="pt-3 pt-md-4 pt-lg-5 px-3 px-md-5">
             <img src="{{$image}}" alt="{{$title}} " width="100%" >
         </div>
-         <p>{{$text}}</p>
+         <p class="py-3 py-md-4 py-lg-5 m-0">{{$text}}</p>
     </div>
 </div>
--- resources/views/components/simple-two-side-card.blade.php
+++ resources/views/components/simple-two-side-card.blade.php
@@ -1,10 +1,10 @@
-<div class="container bg-transparent " id="SimpleTwoSideCard">
+<div class="container bg-transparent" id="SimpleTwoSideCard">
     <div class="overflow-hidden row">
-        <div class="py-2 px-3 px-md-4 px-xl-5 col-7 d-flex justify-content-center flex-column text-side">
+        <div class="text-left pb-0 pt-4 px-4 py-sm-2 px-xl-5 col-sm-7 left d-flex justify-content-center flex-column text-side">
             <h2 class="title">{{$title}}</h2>
             <p class="pt-1 m-0 text">{{$text}}</p>
         </div>
-        <div class="col-5 p-0 image-side">
+        <div class="col-sm-5 right p-4 py-2 p-sm-0 image-side">
             <img src="{{$image}}" alt="{{$title}}" class="d-block w-100 h-100"style="object-fit: cover;">
         </div>
     </div>
