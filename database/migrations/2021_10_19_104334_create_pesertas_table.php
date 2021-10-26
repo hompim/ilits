aaee1fab94a8cd170c9c5555afdab3de6b0e39f1
isabella2807 <putriisabella1007@gamil.com>
2021-10-26 15:07:34 +0700

    edit tabel pesertas

--- database/migrations/2021_10_19_104334_create_pesertas_table.php
+++ database/migrations/2021_10_19_104334_create_pesertas_table.php
@@ -18,14 +18,14 @@ class CreatePesertasTable extends Migration
             // $table->unsignedInteger('user_id');
             $table->string('asal_sekolah');
             $table->string('asal_daerah');
-            $table->unsignedBigInteger('forda_id');
+            //$table->unsignedBigInteger('forda_id');
             $table->string('no_wa');
             $table->string('bukti_bayar')->default(null)->nullable();
             $table->enum('status', ['pending', 'pending_pembayaran', 'aktif', 'ditolak', 'tolak_pembayaran', 'salah_forda'])->default('pending');
             $table->string('kartu_pelajar')->default(null)->nullable();
             $table->enum('pilihan_tryout', ['saintek', 'soshum'])->default('saintek');
             // $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('forda_id')->references('id')->on('forda')->onDelete('cascade')->onUpdate('cascade');
+            //$table->foreign('forda_id')->references('id')->on('forda')->onDelete('cascade')->onUpdate('cascade');
             $table->string('username_tryout')->nullable();
             $table->string('password_tryout')->nullable();
             $table->string('token_tryout')->nullable();
