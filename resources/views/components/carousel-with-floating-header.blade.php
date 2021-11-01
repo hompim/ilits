f0714d82f462fb16c09ce568f0eff0b0bee04c62
christhopermarcelino <christhopermarcelino@gmail.com>
2021-11-01 21:17:34 +0700

    update `CarouselWithFloatingHeader` component style

--- resources/views/components/carousel-with-floating-header.blade.php
+++ resources/views/components/carousel-with-floating-header.blade.php
@@ -1,4 +1,4 @@
-<div class="container px-5 py-5 position-relative carouselWithFloatingHeader">
+<div class="container px-5 pt-5 position-relative CarouselWithFloatingHeader">
     <div class="top-0 px-5 py-3 floating-header position-absolute">
         <h2>{{ $headerTitle }}</h2>
     </div>
@@ -31,13 +31,13 @@
         </div>
         @endfor
     </div>
-    <a class="position-absolute carousel-control" href="#facilityCarousel" role="button" data-slide="prev">
+    {{-- <a class="position-absolute carousel-control" href="#facilityCarousel" role="button" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
     </a>
     <a class="position-absolute carousel-control" href="#facilityCarousel" role="button" data-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
-    </a>
+    </a> --}}
 </div>
 </div>
