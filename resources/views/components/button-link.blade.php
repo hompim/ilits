59bdfbfe4c5d150eae5dbfaadc31a427c19ac27b
christhopermarcelino <christhopermarcelino@gmail.com>
2021-11-01 20:31:12 +0700

    update `ButtonLink` component style

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -1,6 +1,8 @@
-<a href="{{$href}}" {{$attributes->merge(['class' => 'btn button-link border-0 px-5 py-3 d-flex justify-content-center
-    align-items-center'])}}
-    style='background: linear-gradient({{$angle}},
-    {{$colorFrom}}, {{$colorTo}}); backdrop-filter: blur({{$checkIsBlur()}})' target='{{$checkIsExternalLink()}}'>
-    {{$slot}}
+<a href="{{ $href }}"
+  {{ $attributes->merge([
+    'class' => "btn button-link border-0 px-5 py-3 d-flex justify-content-center
+    align-items-center",
+]) }}
+  target="{{ $checkIsExternalLink() }}">
+  {{ $slot }}
 </a>
