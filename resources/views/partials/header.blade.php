0d2b9cbac126fce8869232ac320b72e54c397a6b
christhopermarcelino <christhopermarcelino@gmail.com>
2021-11-14 21:58:08 +0700

    update header design

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,52 @@
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top navbar-slide-nav">
+  <section class="container">
+    <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse w-75" id="navbarNavAltMarkup">
+      <div class="close-mobile d-none align-items-center justify-content-between mb-4">
+        <a href="">
+          <img src="{{ asset('img/icons/logo-small.png') }}" class="logo-small" alt="Ini Lho ITS! 2022">
+        </a>
+        <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="fa fa-2x border-0 fa-chevron-left" aria-hidden="true"></i>
+        </button>
+      </div>
+      <div class="mr-auto navbar-nav">
+        <div class="d-flex align-items-center">
+          <img src="{{ asset('img/icons/home.svg') }}" class="img-responsive img-fluid mr-3" alt="Home">
+          <a class="mx-4 nav-link" href="#">Home</a>
+        </div>
+        <div class="d-flex align-items-center">
+          <img src="{{ asset('img/icons/eventual.svg') }}" class="img-responsive img-fluid mr-3" alt="Eventual">
+          <a class="mx-4 nav-link" href="#">Eventual</a>
+        </div>
+        <div class="d-flex align-items-center">
+          <img src="{{ asset('img/icons/fakultas.svg') }}" class="img-responsive img-fluid mr-3" alt="Fakultas">
+          <a class="mx-4 nav-link" href="#">Fakultas</a>
+        </div>
+        <div class="d-flex align-items-center">
+          <img src="{{ asset('img/icons/prestasi.svg') }}" class="img-responsive img-fluid mr-3" alt="Prestasi">
+          <a class="mx-4 nav-link" href="#">Prestasi</a>
+        </div>
+        <div class="d-flex align-items-center">
+          <img src="{{ asset('img/icons/fasilitas.svg') }}" class="img-responsive img-fluid mr-3" alt="Fasilitas">
+          <a class="mx-4 nav-link" href="#">Fasilitas</a>
+        </div>
+        <div class="d-flex align-items-center">
+          <img src="{{ asset('img/icons/alumni.svg') }}" class="img-responsive img-fluid mr-3" alt="Alumni">
+          <a class="mx-4 nav-link" href="#">Alumni</a>
+        </div>
+        <div class="d-flex align-items-center">
+          <img src="{{ asset('img/icons/beasiswa.svg') }}" class="img-responsive img-fluid mr-3" alt="Beasiswa">
+          <a class="mx-4 nav-link" href="#">Beasiswa</a>
+        </div>
+      </div>
+      <div class="ml-auto border-top">
+        <a class="nav-link" href="#">Masuk</a>
+      </div>
+    </div>
+  </section>
+</nav>
