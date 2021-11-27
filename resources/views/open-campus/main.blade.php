0cbff14a450549b75a673436ff3b16671fb7c78c
Muhammad Ridho Waradana <muhammadridhowaradana@gmail.com>
2021-11-28 01:08:42 +0700

    register-fnd, event-regist, dashboard-fnd

---
+++ resources/views/open-campus/main.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="{{ asset('css/partials/header.css') }}">
+
+    @yield('stylesheet')
+
+    <title>Open Campus | {{ $title }}</title>
+</head>
+
+<body>
+
+    @include('partials.header')
+
+
+    @yield('open-campus-content')
+
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
---
+++ resources/views/open-campus/register-fnd.blade.php
@@ -0,0 +1,31 @@
+@extends('open-campus.main')
+
+@section('stylesheet')
+  <link rel="stylesheet" href="{{ asset('css/open-campus/register-fnd.css') }}">
+@endsection
+
+@section('open-campus-content')
+  <!-- header section start -->
+  <header class="content d-flex text-center justify-content-center align-items-center">
+      <div class="container">
+          <div class="row">
+              <div class="col">
+                  <h1 class="content-title">Faculty and<br> Department Day</h1>
+              </div>
+          </div>
+          <div class="row my-3">
+              <div class="col">
+                  <h5 class="content-text"> <span class="bg-white highlight">100.000 students</span> have already registered!</h5>
+              </div>
+          </div>
+          <div class="row my-4">
+              <div class="col">
+                  <a href="#" class="btn-content"> Daftar Sekarang! </a>
+              </div>
+          </div>
+      </div>
+  </header>
+  <!-- header end -->
+@endsection
+
+
